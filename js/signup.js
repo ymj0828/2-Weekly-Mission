c8619a2c4cfa16c28bd72eaf52510d664f38109a
ymj0828 <ymj369@nate.com>
2023-11-12 16:29:26 +0900

    feat: 비밀번호 상태 아이콘 클릭 시 input type 변경

--- js/signup.js
+++ js/signup.js
@@ -79,3 +79,24 @@ form.addEventListener("submit", (e) => {
     window.location.href = "./folder";
   }
 });
+
+const visiblePasswordIcon = document.querySelectorAll(".input-wrap > img");
+
+visiblePasswordIcon.forEach((iconEle) => {
+  iconEle.addEventListener("click", () => {
+    const currentType = iconEle.previousElementSibling.getAttribute("type");
+    const inputType = currentType === "password" ? "text" : "password";
+    const imgSrc =
+      currentType === "password"
+        ? "./img/icon/eye_on_icon.svg"
+        : "./img/icon/eye_off_icon.svg";
+    const imgAlt =
+      currentType === "password"
+        ? "현재 비밀번호가 보이는 상태 아이콘"
+        : "현재 비밀번호가 보이지 않는 상태 아이콘";
+
+    iconEle.previousElementSibling.setAttribute("type", inputType);
+    iconEle.setAttribute("src", imgSrc);
+    iconEle.setAttribute("alt", imgAlt);
+  });
+});
