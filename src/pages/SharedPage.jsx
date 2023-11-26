a9e6fb7a36631e1c552dc8f9d7bd8a534e4c1c29
ymj0828 <ymj369@nate.com>
2023-11-26 23:24:25 +0900

    feat: Shared 페이지 생성

--- src/pages/SharedPage.jsx
+++ src/pages/SharedPage.jsx
@@ -1,1 +1,17 @@
-export default function SharedPage() {}
+import Header from "../components/header/Header";
+import FolderInfo from "../components/folder_info/FolderInfo";
+import LinkList from "../components/link_list/LinkList";
+import Footer from "../components/footer/Footer";
+
+const SharedPage = () => {
+  return (
+    <>
+      <Header />
+      <FolderInfo />
+      <LinkList />
+      <Footer />
+    </>
+  );
+};
+
+export default SharedPage;
