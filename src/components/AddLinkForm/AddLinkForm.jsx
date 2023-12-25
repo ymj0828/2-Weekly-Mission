cd396e7b52f5dd5e1800e8624328ef6b426b412b
ymj0828 <ymj369@nate.com>
2023-12-25 16:38:34 +0900

    refactor: AddLinkForm 컴포넌트 css 모듈로 변경

--- src/components/AddLinkForm/AddLinkForm.jsx
+++ src/components/AddLinkForm/AddLinkForm.jsx
@@ -1,15 +1,22 @@
-import * as S from "./AddLinkForm.style";
-import linkIcon from "../../assets/link-icon.svg";
+import styles from "./AddLinkForm.module.css";
+import classNames from "classnames/bind";
+import iconLink from "../../assets/icon/icon-link.svg";
+
+const cx = classNames.bind(styles);
 
 const AddLinkForm = () => {
   return (
-    <S.AddLinkForm>
-      <S.FormWrap>
-        <img src={linkIcon} alt="링크 추가하기 아이콘" />
-        <S.Input type="text" placeholder="링크를 추가해 보세요" />
-        <S.Button>추가하기</S.Button>
-      </S.FormWrap>
-    </S.AddLinkForm>
+    <div className={cx("container")}>
+      <form className={cx("form")}>
+        <img className={cx("icon")} src={iconLink} alt="링크 추가하기 아이콘" />
+        <input
+          className={cx("input")}
+          type="text"
+          placeholder="링크를 추가해 보세요"
+        />
+        <button className={cx("button")}>추가하기</button>
+      </form>
+    </div>
   );
 };
 export default AddLinkForm;
