a5b0156ab981c37e3a89dbdbaa31277a502f4aff
ymj0828 <ymj369@nate.com>
2024-01-10 17:15:20 +0900

    chore: 초기 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/global_styles.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
